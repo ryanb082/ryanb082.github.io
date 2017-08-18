d09c8d4f88267b234ac3bf301a5bc338f204359f
Ryan <ryanbent01@gmail.com>
2017-08-17 19:06:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <!-- Latest compiled and minified CSS -->
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!--End Bootstrap -->
+        <!-- Animate.css -->
+        <link rel="stylesheet" href="css/animate.min.css"
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/main.css">
+
+    <!-- animation -->
+    <link rel="stylesheet" href="css/usa-jumpers-style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/85/three.min.js"></script>
+    <script src="js/usa-jumpers.min.js"></script>
+    <!-- animation ends -->
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- For Bootstrap -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- End Bootstrap -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body data-spy="scroll" data-target="#myNavbar" data-offset="70">
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+<div class="bg-img">
+<div id="myNavbar" class="navbar navbar-style" role="navigation">
+    <div class="container">
+
+      <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+         <a class="navbar-brand" href="#">
+          <div class="logo">
+            <img src="img/DigitalDeliveryLogo2.png" class="img-responsive" width="100px" height="100px"/>
+          </div>
+        </a>
+
+
+      </div>
+
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="#originations">Digital Originations</a></li>
+          <li><a href="#experience">Customer Experience</a></li>
+          <li><a href="#operations">Digitized Operations</a></li>
+          <li><a href="#cloud-doc">Cloud Document Managment</a></li>
+          <li><a href="#review">Reviews</a></li>
+          <li><a href="http://digitaldelivery.tech/contact.php">Contact Us</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+  </div>
+</div>
+
+<!-- Hero section -->
+
+
+  <section id="hero">
+    <div class="container">
+        <div class="row">
+          <div class="hero-content col-md-6">
+            <h1 id="hero-headline">IT SAVES YOU TIME, MONEY, AND HEADACHES!</h1>
+            <p>Digital Delivery Inc. is the premiere digital platform to Delivery funds,
+              documents, and data to the entire banking and financial  services industry. We deliver real time
+              data interface and document executions as soon as you need them.
+            </p>
+            <a href="http://digitaldelivery.tech/contact.php">
+            <div class="btn btn-primary">Learn More</div></a>
+          </div>
+          
+          <div class="col-md-6">
+            <div id="usa-jumpers-div" style="width: 800px;height: 500px;">
+              <div id="spinner-container">
+                <div class="loader" ></div>
+              </div>
+              <div id="usa-jumpers-container"></div>
+            </div>
+            <!--<img src="img/CloudImage.png"/>-->
+          </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<!-- Digital Delivery won't just meet your needs -->
+<section>
+  <div class="container">
+    <div class="row circle-image">
+      <div class"col-md-6">
+        <img class="img-responsive center-block" src="img/Ellipse2.png">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class"col-md-6">
+        <h3 class="text-center" id ="callout">Digital Delivery won't just meet your needs;
+          </h3>
+          <h4 class="text-center" id="copy">It'll surpass them</h4>
+      </div>
+    </div>
+  </div>
+</section>
+  <!-- Digitized Originations -->
+
+  <section id="originations">
+    <div class="container">
+ 
+        <div class="row">
+          <div class="col-md-6 wow bounceInLeft">
+            <h1>Digitized Originations</h1>
+            <p>We are able to customize kiosks specifically for your business and required processes. Digital Delivery can easily populate and transmit reports, forms, and documents from your internal systems to any other application you use
+            </p>
+          </div>
+
+          <div class="col-md-6 wow bounceInRight" align="center">
+            <img src="img/digitized-originations.png" class="img-responsive"/>
+        </div>
+    </div>
+  </section>
+
+  <!-- END Originations -->
+
+
+  <!-- EXPERIENCE -->
+  <section id="experience">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 wow slideInLeft">
+            <img src="img/customer-experience.png" class="img-responsive"/>
+        </div>
+      <div class="col-md-6 wow slideInRight">
+        <h1>Customer Experience</h1>
+        <p>Don’t wait. Start executing documents electronically for your customer, and gives them the convenience and security they’ll need to become your customer for life. </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- END EXPERIENCE -->
+
+   <!-- ORIGINATIONS -->
+
+  <section id="operations">
+    <div class="container">
+      <div class="row">
+         <div class="col-md-6 wow fadeIn" data-wow-delay="0.3s">
+            <h1>Digitized Operations</h1>
+            <p>Automated funding. Streamlined investor loan purchase. Ensured regulation requirements. Digital Delivery makes sending your loans to the secondary market electrifying with the ability to handle all naming conventions, work across all common platforms, and multi-factor identifications.
+            </p>
+        </div>
+      <div class="col-md-6 wow fadeIn" data-wow-delay="0.3s">
+          <img src="img/digitized-operations.png" class="img-responsive">
+        </div>
+    </div>
+    </div>
+  </section>
+
+  <!-- END ORIGINATIONS -->
+
+   <!-- CLOUD DOC -->
+
+  <section id="cloud-doc">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-6 wow slideInLeft">
+            <img src="img/cloud-doc-managment.png">
+          </div>
+      <div class="col-md-6 wow slideInRight">
+        <h1>Cloud Document Managment</h1>
+        <p>Cloud-based loan retrieval and retention. We’ve built a system specifically for the smooth and secure loan retention with intuitive controls and lighting speeds. Never slow down your workday searching for files again!
+        </p>
+    </div>
+  </div>
+  </div>
+  </section>
+
+  <section id="review">
+    <div class="container">
+      <div class="row">
+        <div class"col-md-6">
+          <img class="img-responsive center-block" src="img/Ellipse2.png">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 review-row">
+          <h3 class="text-center" id ="review-callout">What Our Users Think About Digital Delivery</h3>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row ">
+        <div class="col-md-3 wow fadeIn no-padding" data-wow-delay="0.3s">
+          <div class="review-box" id="review-box-1-dimensions"->
+            <h4>John</h4>
+            <h5>Settlement Agent</h5>
+            <p>"Accurate funds have definitely made for smooth closings.”</p>
+        </div>
+      </div>
+    <div class="col-md-3 wow fadeIn no-padding" data-wow-delay="0.4s">
+          <div class="review-box" id="review-box-2-dimensions">
+            <h4>Eric</h4>
+            <h5>Real Estate Agent</h5>
+            <p>"I used the Loan Kiosk in my office to get faster loan estimates than any other method. Easy sales contracts equals easy sales.”</p>
+        </div>
+      </div>
+      <div class="col-md-3 wow fadeIn no-padding" data-wow-delay="0.5s">
+          <div class="review-box" id="review-box-3-dimensions">
+            <h4>Dave</h4>
+            <h5>Loan Officer</h5>
+            <p>“I actually received four customer 
+              referrals from my last e-closing alone!” 
+            </p>
+        </div>
+      </div>
+      <div class="col-md-3 wow fadeIn no-padding" data-wow-delay="0.6s">
+          <div class="review-box" id="#review-box-4-dimensions">
+            <h4>Sally</h4>
+            <h5>New homeowner</h5>
+            <p>“Best, easiest closing of my life.” 
+            </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- FOOTER -->
+
+  <section id="footer">
+    <div class="container">
+
+      <div class="row">
+          <div class="col-md-4">
+            <img src="img/bottom-logo.png" class="bottom-logo"/>
+          </div>
+          <div class="col-md-3 col-md-offset-5">
+
+            <ul class="nav navbar-nav footer">
+              <div class='row'>
+                  <li><a href="#originations">Digitized Originations</a></li>
+                </div>
+                <div class='row'>
+                  <li><a href="#experience">Customer Experince</a></li>
+                </div>
+                <div class='row'>
+                  <li><a href="#operations">Digitized Operations</a></li>
+                </div>
+                <div class="row">
+                  <li><a href="#cloud-doc">Cloud Document Managment</a></li>
+                </div>
+                <div class="row">
+                  <li><a href="#review">Reviews</a></li>
+                </div>
+                <div class="row">
+                  <li><a href="#contact">Contact Us</li>
+                </div>
+              </div>
+            </ul>
+        </div>
+  </section>
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ mail.php
@@ -0,0 +1,4 @@
+<?php $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phoneNumber = $_POST['phone number'];
+  $occupation = $_POST['occupation'];
